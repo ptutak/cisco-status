76bdab84213c455cba4f7536633ce475b7007e49
Piotr Tutak <piotr.tutak@getindata.com>
2024-06-27 00:55:19 +0200

    fixing

--- tests/unit/cisco_status/resolver_test.py
+++ tests/unit/cisco_status/resolver_test.py
@@ -1,6 +1,6 @@
 from cisco_status.const import HSRPState
 from cisco_status.credentials import RouterCredentials
-from cisco_status.resolver import RouterHSRPResolver, Router, DesiredHSRPConfig
+from cisco_status.resolver import DesiredHSRPConfig, Router, RouterHSRPResolver
 
 
 class MyRouter(Router):
@@ -13,19 +13,37 @@ Gi0/0/1     2    105 P Standby 82.0.0.11       local           82.0.0.9"""
     def from_credentials(cls, credentials: RouterCredentials) -> Router:
         return super().from_credentials(credentials)
 
-def test_resolver():
-    resolver = RouterHSRPResolver("CE1", MyRouter(), [
-        DesiredHSRPConfig(name="CE1", interface="Gi0/0/1", group=1, state=HSRPState.Active),
-        DesiredHSRPConfig(name="CE1", interface="Gi0/0/1", group=2, state=HSRPState.Standby)
-    ])
 
-    assert resolver.resolve_router_config() == {'CE1': {'Gi0/0/1': [{'group': 'Group 1', 'status': 'Pass'}, {'group': 'Group 2', 'status': 'Pass'}]}}
+def test_resolver():
+    resolver = RouterHSRPResolver(
+        "CE1",
+        MyRouter(),
+        [
+            DesiredHSRPConfig(name="CE1", interface="Gi0/0/1", group=1, state=HSRPState.Active),
+            DesiredHSRPConfig(name="CE1", interface="Gi0/0/1", group=2, state=HSRPState.Standby),
+        ],
+    )
+
+    assert resolver.resolve_router_config() == {
+        "CE1": {"Gi0/0/1": [{"group": "Group 1", "status": "Pass"}, {"group": "Group 2", "status": "Pass"}]}
+    }
 
 
 def test_resolver_fail():
-    resolver = RouterHSRPResolver("CE1", MyRouter(), [
-        DesiredHSRPConfig(name="CE1", interface="Gi0/0/1", group=1, state=HSRPState.Standby),
-        DesiredHSRPConfig(name="CE1", interface="Gi0/0/1", group=2, state=HSRPState.Standby)
-    ])
+    resolver = RouterHSRPResolver(
+        "CE1",
+        MyRouter(),
+        [
+            DesiredHSRPConfig(name="CE1", interface="Gi0/0/1", group=1, state=HSRPState.Standby),
+            DesiredHSRPConfig(name="CE1", interface="Gi0/0/1", group=2, state=HSRPState.Standby),
+        ],
+    )
 
-    assert resolver.resolve_router_config() == {'CE1': {'Gi0/0/1': [{'group': 'Group 1', 'status': 'Fail - No longer Standby'}, {'group': 'Group 2', 'status': 'Pass'}]}}
+    assert resolver.resolve_router_config() == {
+        "CE1": {
+            "Gi0/0/1": [
+                {"group": "Group 1", "status": "Fail - No longer Standby"},
+                {"group": "Group 2", "status": "Pass"},
+            ]
+        }
+    }
